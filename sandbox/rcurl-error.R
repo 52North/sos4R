5333c176470ec7a33c5d1959774d334ff31a97e7
nuest <d.nuest@52north.org>
2013-04-22 17:12:49 +0000

    reproducible example for rcurl error

--- sandbox/rcurl-error.R
+++ sandbox/rcurl-error.R
@@ -2,12 +2,30 @@
 
 # Fehler: Unhandled case for the value of curl_easy_setopt (R type = 19, option 10002)
 
-library("RCurl")
 library("sos4R")
 
 mySOS <- SOS(url = "http://v-swe.uni-muenster.de:8080/WeatherSOS/sos")
 mySOS@curlOptions
 mySOS@curlHandle
+
+sosCapabilitiesDocumentOriginal(sos = mySOS)
+# does NOT work
+
+getCapRequest <- '<sos:GetCapabilities xsi:schemaLocation="http://www.opengis.net/sos/1.0 http://schemas.opengis.net/sos/1.0.0/sosAll.xsd" service="SOS" xmlns:sos="http://www.opengis.net/sos/1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:ogc="http://www.opengis.net/ogc">
+ <ows:AcceptVersions>
+  <ows:Version>1.0.0</ows:Version>
+ </ows:AcceptVersions>
+ <ows:Sections>
+  <ows:Section>All</ows:Section>
+ </ows:Sections>
+ <ows:AcceptFormats>
+ <ows:OutputFormat>text/xml</ows:OutputFormat>
+ </ows:AcceptFormats>
+</sos:GetCapabilities>'
+
+postForm(uri = "http://v-swe.uni-muenster.de:8080/WeatherSOS/sos", request = getCapRequest, style = "post",
+				 .opts = mySOS@curlOptions, curl = mySOS@curlHandle)
+# works!
 
 response <- postForm(uri = "http://v-swe.uni-muenster.de:8080/WeatherSOS/sos",
 				 request = '<sos:DescribeSensor xsi:schemaLocation="http://www.opengis.net/sos/1.0 http://schemas.opengis.net/sos/1.0.0/sosAll.xsd" service="SOS" outputFormat="text/xml;subtype=&quot;sensorML/1.0.1&quot;" version="1.0.0" xmlns:sos="http://www.opengis.net/sos/1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
