7c275237dbee054facfdbe327d0ceead221eaf73
Daniel Nüst <d.nuest@52north.org>
2015-01-23 12:39:59 +0100

    adjusted copyright statements (year)

--- R/SOS_20-methods-impl.R
+++ R/SOS_20-methods-impl.R
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (C) 2013 by 52°North                                               #
+# Copyright (C) 2015 by 52 North                                               #
 # Initiative for Geospatial Open Source Software GmbH                          #
 #                                                                              #
 # Contact: Andreas Wytzisk                                                     #
