61637454168f4b76791d822e7ced4d35b14ec336
nuest <daniel.nuest@uni-muenster.de>
2019-01-03 17:42:19 +0100

    add release question

--- R/Development.R
+++ R/Development.R
@@ -1,6 +1,6 @@
 release_questions <- function() {
   c(
-    "Have you updated the version and date in man/sos4R-package.Rd"
-    #"one more question"
+    "Did you update the version and date in 'man/sos4R-package.Rd' ?",
+    "Did you update the pkgdown site with 'pkgdown::build_site()' ?"
   )
 }
