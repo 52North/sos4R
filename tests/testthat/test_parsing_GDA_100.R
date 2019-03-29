e4f197f1f4be1103d683323e3cd57b49f34000ae
EHJ-52n <e.h.juerrens@52north.org>
2019-03-29 10:58:11 +0100

    Add test for parsing GetDataAvailabilityResponse::dataAvailabilityMember

--- tests/testthat/test_parsing_GDA_100.R
+++ tests/testthat/test_parsing_GDA_100.R
@@ -60,3 +60,52 @@ test_that("parseGetDataAvailabilityResponse() returns an empty list if an empty 
   expect_true(is.list(.dams))
   expect_true(length(.dams) == 0)
 })
+
+test_that("parseGetDataAvailabilityResponse() returns a correct parsed list of DataAvailabilityMembers if a response is received", {
+  webmockr::stub_request("get", uri = "http://example.com/sos-list-phenomena?service=SOS&request=GetCapabilities&acceptVersions=2.0.0&sections=All&acceptFormats=text%2Fxml") %>%
+    webmockr::wi_th(
+      headers = list("Accept" = "application/xml")
+    ) %>%
+    webmockr::to_return(
+      status = 200,
+      body = readr::read_file("../responses/Capabilities_200_Example.com.xml"),
+      headers = list("Content-Type" = "application/xml")
+    )
+
+  sos <- SOS(version = sos200_version, url = "http://example.com/sos-list-phenomena", binding = "KVP")
+  .doc <- parseXmlSnippet(readr::read_file("../responses/GetDataAvailability_100_Example.com_single.xml"))
+  .dams <- parseGetDataAvailabilityResponse(obj = .doc, sos)
+
+  expect_false(is.null(.dams))
+  expect_true(is.list(.dams))
+  expect_true(length(.dams) == 1)
+
+  # <gda:dataAvailabilityMember gml:id="dam_1">
+  #   <gda:procedure xlink:href="ws2500" xlink:title="ws2500"/>
+  #   <gda:observedProperty xlink:href="WindDirection" xlink:title="WindDirection"/>
+  #   <gda:featureOfInterest xlink:href="elv-ws2500" xlink:title="ELV WS2500"/>
+  #   <gda:phenomenonTime>
+  #     <gml:TimePeriod gml:id="tp_1">
+  #       <gml:beginPosition>2019-03-01T00:30:00.000Z</gml:beginPosition>
+  #       <gml:endPosition>2019-03-28T23:45:00.000Z</gml:endPosition>
+  #     </gml:TimePeriod>
+  #   </gda:phenomenonTime>
+  #  </gda:dataAvailabilityMember>
+  #
+  # new("DataAvailabilityMember",
+  #     procedure = procedure,
+  #     observedProperty = observedProperty,
+  #     featureOfInterest = featureOfInterest,
+  #     phenomenonTime = phenomenonTime)
+
+  .dam <- .dams[[1]]
+
+  expect_equal(.dam@procedure, "ws2500", info = "procedure")
+  expect_equal(.dam@observedProperty, "WindDirection", info = "observedProperty")
+  expect_equal(.dam@featureOfInterest, "elv-ws2500", info = "featureOfInterest")
+  expect_true(is.object(.dam@phenomenonTime))
+  expect_true(isS4(.dam@phenomenonTime))
+  expect_true(is(.dam@phenomenonTime,"GmlTimePeriod"))
+  expect_equal(.dam@phenomenonTime@beginPosition@time, strptime("2019-03-01T00:30:00.000Z", format = "%Y-%m-%dT%H:%M:%OS"), info = "phenomenonTime::start")
+  expect_equal(.dam@phenomenonTime@endPosition@time, strptime("2019-03-28T23:45:00.000Z", format = "%Y-%m-%dT%H:%M:%OS"), info = "phenomenonTime::end")
+})
