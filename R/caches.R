16d0975e733cdf2d8ce33a836997a3aec717cdec
Daniel Nüst <daniel.nuest@uni-muenster.de>
2020-04-22 11:11:00 +0200

    fix final warning in CHECK

--- R/caches.R
+++ R/caches.R
@@ -1,7 +1,4 @@
-# a global variable to cache objects from documents when parsing GetDataAvailability responses
-#cacheVariables <- c("featureCache", "phenTimeCache", "timeObjectCache")
-#utils::globalVariables(cacheVariables)
-#utils::suppressForeignCheck(cacheVariables)
-
+# store caches in an environment, not as global variables assigned with <<-
 # https://stackoverflow.com/questions/41954302/where-to-create-package-environment-variables
 assign(x = "sos4R_caches", value = new.env())
+utils::globalVariables("sos4R_caches")
